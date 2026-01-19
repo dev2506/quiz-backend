9e0619744ee0a88ebedeeeaf084b03eddbb39961
Devarsh Bhavsar <dbhavsar2506@gmail.com>
2026-01-19 18:00:15 +0530

    added user schema for mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  //TODO: Move mongo db url to env
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
